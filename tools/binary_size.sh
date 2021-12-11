900c7f5f7c72860703196e397b21e1ac7a5e40e5
Jeff McGlynn <jeff@jeffmcglynn.com>
2021-12-11 21:44:56 +0000

    Improve coverage tool to use Bazel built-in method
    Additionally, augment the `binary_size.sh` tool to also report the
    renderer size.

--- tools/binary_size.sh
+++ tools/binary_size.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 cd "${0%/*}"
 
-bazel build -c opt //src/svg/xml:xml_tool
+bazel build -c opt //src/svg/xml:xml_tool //src/renderer:renderer_tool
+
 du -h ../bazel-bin/src/svg/xml/xml_tool
+du -h ../bazel-bin/src/renderer/renderer_tool
