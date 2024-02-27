740d220c1adb91cf556aee7c3f182b688451b5c1
Jeff McGlynn <jeff@mcglynn.dev>
2024-02-27 14:07:22 -0800

    Add comments to mermaid filter

--- tools/support/doxygen_filter_mermaid.sh
+++ tools/support/doxygen_filter_mermaid.sh
@@ -8,13 +8,18 @@
 #
 #   FILTER_PATTERNS = *.md=tools/support/doxygen_filter_mermaid.sh
 #
-sed -E '/^```mermaid$/,/^```$/ {
-/^```mermaid$/ {
+sed -E '# Match a range starting with ```mermaid and ending with ```
+/^```mermaid$/,/^```$/ {
+  # Replace the ```mermaid part with a <pre> tag
+  /^```mermaid$/ {
     s/^```mermaid/<pre class="mermaid">/
+    # Move to next line line
     n
-}
-/^```$/ {
+  }
+  # Replace the end marker with </pre>
+  /^```$/ {
     s/^```/<\/pre>/
+    # Move to next line line
     n
-}
+  }
 }' "${1}"
