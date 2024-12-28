5e9f33a06e30ead6d3008ec6c23b770930900c79
Jeff McGlynn <jeff@mcglynn.dev>
2024-12-28 10:28:52 -0800

    Add more test coverage for XMLNode

--- donner/base/xml/tests/XMLNode_tests.cc
+++ donner/base/xml/tests/XMLNode_tests.cc
@@ -1,3 +1,5 @@
+#include "donner/base/xml/XMLNode.h"
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
@@ -12,6 +14,7 @@ using testing::AllOf;
 using testing::ElementsAre;
 using testing::Eq;
 using testing::Ne;
+using testing::Optional;
 
 // TODO: Add an ErrorHighlightsText matcher
 
@@ -41,9 +44,147 @@ protected:
     return root.firstChild();
   }
 
+  // Helper to create a basic document with a single element
+  XMLDocument createTestDocument() {
+    XMLDocument doc;
+    XMLNode element = XMLNode::CreateElementNode(doc, "test");
+    doc.root().appendChild(element);
+    return doc;
+  }
+
 private:
   XMLDocument document_;
 };
+
+TEST_F(XMLNodeTests, CreateElementNode) {
+  XMLDocument doc;
+  XMLNode element = XMLNode::CreateElementNode(doc, "test");
+
+  EXPECT_EQ(element.type(), XMLNode::Type::Element);
+  EXPECT_THAT(element.tagName(), Eq("test"));
+  EXPECT_THAT(element.value(), Eq(std::nullopt));
+}
+
+TEST_F(XMLNodeTests, CreateDataNode) {
+  XMLDocument doc;
+  const char* testValue = "Hello, world!";
+  XMLNode node = XMLNode::CreateDataNode(doc, testValue);
+
+  EXPECT_EQ(node.type(), XMLNode::Type::Data);
+  EXPECT_THAT(node.tagName(), Eq(""));
+  EXPECT_THAT(node.value(), Optional(Eq(testValue)));
+}
+
+TEST_F(XMLNodeTests, TreeManipulation) {
+  XMLDocument doc;
+  XMLNode parent = XMLNode::CreateElementNode(doc, "parent");
+  XMLNode child1 = XMLNode::CreateElementNode(doc, "child1");
+  XMLNode child2 = XMLNode::CreateElementNode(doc, "child2");
+
+  // Test appendChild
+  parent.appendChild(child1);
+  parent.appendChild(child2);
+
+  EXPECT_THAT(parent.firstChild(), Optional(Eq(child1)));
+  EXPECT_THAT(parent.lastChild(), Optional(Eq(child2)));
+  EXPECT_THAT(child1.nextSibling(), Optional(Eq(child2)));
+  EXPECT_THAT(child2.previousSibling(), Optional(Eq(child1)));
+  EXPECT_THAT(child1.parentElement(), Optional(Eq(parent)));
+  EXPECT_THAT(child2.parentElement(), Optional(Eq(parent)));
+
+  // Test insertBefore
+  XMLNode child3 = XMLNode::CreateElementNode(doc, "child3");
+  parent.insertBefore(child3, child2);
+
+  EXPECT_THAT(child1.nextSibling(), Optional(Eq(child3)));
+  EXPECT_THAT(child3.nextSibling(), Optional(Eq(child2)));
+
+  // Test removeChild
+  parent.removeChild(child3);
+  EXPECT_THAT(child1.nextSibling(), Optional(Eq(child2)));
+  EXPECT_THAT(child3.parentElement(), Eq(std::nullopt));
+
+  // Test replaceChild
+  XMLNode replacement = XMLNode::CreateElementNode(doc, "replacement");
+  parent.replaceChild(replacement, child1);
+
+  EXPECT_THAT(parent.firstChild(), Optional(Eq(replacement)));
+  EXPECT_THAT(child1.parentElement(), Eq(std::nullopt));
+}
+
+TEST_F(XMLNodeTests, AttributeHandling) {
+  auto maybeRoot = parseAndGetFirstNode(R"(<root attr1="value1" attr2="value2"></root>)");
+  ASSERT_TRUE(maybeRoot.has_value());
+  XMLNode root = std::move(maybeRoot.value());
+
+  // Test attribute existence
+  EXPECT_TRUE(root.hasAttribute("attr1"));
+  EXPECT_TRUE(root.hasAttribute("attr2"));
+  EXPECT_FALSE(root.hasAttribute("attr3"));
+
+  // Test attribute values
+  EXPECT_THAT(root.getAttribute("attr1"), Optional(Eq("value1")));
+  EXPECT_THAT(root.getAttribute("attr2"), Optional(Eq("value2")));
+  EXPECT_THAT(root.getAttribute("attr3"), Eq(std::nullopt));
+
+  // Test setting attributes
+  root.setAttribute("attr3", "value3");
+  EXPECT_THAT(root.getAttribute("attr3"), Optional(Eq("value3")));
+
+  // Test removing attributes
+  root.removeAttribute("attr1");
+  EXPECT_FALSE(root.hasAttribute("attr1"));
+}
+
+TEST_F(XMLNodeTests, NodeTypes) {
+  XMLDocument doc;
+
+  // Test various node types
+  XMLNode element = XMLNode::CreateElementNode(doc, "element");
+  EXPECT_EQ(element.type(), XMLNode::Type::Element);
+
+  XMLNode data = XMLNode::CreateDataNode(doc, "data");
+  EXPECT_EQ(data.type(), XMLNode::Type::Data);
+
+  XMLNode cdata = XMLNode::CreateCDataNode(doc, "cdata");
+  EXPECT_EQ(cdata.type(), XMLNode::Type::CData);
+
+  XMLNode comment = XMLNode::CreateCommentNode(doc, "comment");
+  EXPECT_EQ(comment.type(), XMLNode::Type::Comment);
+
+  XMLNode doctype = XMLNode::CreateDocTypeNode(doc, "doctype");
+  EXPECT_EQ(doctype.type(), XMLNode::Type::DocType);
+
+  XMLNode pi = XMLNode::CreateProcessingInstructionNode(doc, "target", "value");
+  EXPECT_EQ(pi.type(), XMLNode::Type::ProcessingInstruction);
+
+  XMLNode xmlDecl = XMLNode::CreateXMLDeclarationNode(doc);
+  EXPECT_EQ(xmlDecl.type(), XMLNode::Type::XMLDeclaration);
+}
+
+TEST_F(XMLNodeTests, SourceOffsets) {
+  XMLDocument doc;
+  XMLNode element = XMLNode::CreateElementNode(doc, "test");
+
+  auto start = FileOffset::Offset(42);
+  auto end = FileOffset::Offset(100);
+
+  element.setSourceStartOffset(start);
+  element.setSourceEndOffset(end);
+
+  EXPECT_THAT(element.sourceStartOffset(), Optional(Eq(start)));
+  EXPECT_THAT(element.sourceEndOffset(), Optional(Eq(end)));
+}
+
+TEST_F(XMLNodeTests, NodeEquality) {
+  XMLDocument doc;
+  XMLNode node1 = XMLNode::CreateElementNode(doc, "test");
+  XMLNode node2 = node1;  // NOLINT, Create a copy
+  XMLNode node3 = XMLNode::CreateElementNode(doc, "test");
+
+  EXPECT_EQ(node1, node2);  // Same underlying node
+  EXPECT_NE(node1, node3);  // Different nodes
+}
 
 TEST_F(XMLNodeTests, GetNodeLocation) {
   std::string_view xml = R"(<root><child attr="Hello, world!"></child></root>)";
@@ -57,6 +198,7 @@ TEST_F(XMLNodeTests, GetNodeLocation) {
     EXPECT_THAT(root.tagName(), Eq("root"));
 
     auto location = root.getNodeLocation();
+    ASSERT_THAT(location, Ne(std::nullopt));
 
     // Extract substring from the returned offsets.
     const size_t start = location->start.offset.value();
@@ -74,22 +216,19 @@ TEST_F(XMLNodeTests, GetNodeLocation) {
   {
     EXPECT_EQ(child.type(), XMLNode::Type::Element);
     EXPECT_THAT(child.tagName(), Eq("child"));
+    auto childLocation = child.getNodeLocation();
+    ASSERT_THAT(childLocation, Ne(std::nullopt));
 
-    auto location = root.getNodeLocation();
-    ASSERT_THAT(location, Ne(std::nullopt));
+    const size_t childStart = childLocation->start.offset.value();
+    const size_t childEnd = childLocation->end.offset.value();
+    EXPECT_LT(childStart, childEnd);
+    std::string_view childSubstr = xml.substr(childStart, childEnd - childStart);
 
-    // Extract substring from the returned offsets.
-    const size_t start = location->start.offset.value();
-    const size_t end = location->end.offset.value();
-    EXPECT_LT(start, end);
-    std::string_view nodeSubstr = xml.substr(start, end - start);
-
-    EXPECT_THAT(nodeSubstr, Eq(xml));
+    EXPECT_THAT(childSubstr, Eq(R"(<child attr="Hello, world!"></child>)"));
   }
 }
 
 TEST_F(XMLNodeTests, GetAttributeLocation) {
-  // Setup test XML.
   std::string_view xml = R"(<child attr="Hello, world!"></child>)";
 
   auto maybeChild = parseAndGetFirstNode(xml);
@@ -99,14 +238,12 @@ TEST_F(XMLNodeTests, GetAttributeLocation) {
   auto location = child.getAttributeLocation(xml, "attr");
   ASSERT_THAT(location, Ne(std::nullopt));
 
-  // Extract substring from the returned offsets.
   const size_t start = location->start.offset.value();
   const size_t end = location->end.offset.value();
   EXPECT_LT(start, end);
   std::string_view foundAttribute = xml.substr(start, end - start);
 
-  // Verify correctness.
-  EXPECT_THAT(foundAttribute, testing::Eq(R"(attr="Hello, world!")"));
+  EXPECT_THAT(foundAttribute, Eq(R"(attr="Hello, world!")"));
 }
 
 }  // namespace donner::xml
