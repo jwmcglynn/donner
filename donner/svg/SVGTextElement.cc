e4f2120bd40d6e033b2578879e1c25cbbe205da7
Jeff McGlynn <jeff@mcglynn.dev>
2025-01-12 22:07:51 -0800

    Bump version to 0.1.1

---
+++ donner/svg/SVGTextElement.cc
@@ -0,0 +1,70 @@
+#include "donner/svg/SVGDocument.h"
+#include "donner/svg/SVGSVGElement.h"
+#include "donner/svg/components/PreserveAspectRatioComponent.h"
+#include "donner/svg/components/StylesheetComponent.h"
+#include "donner/svg/components/layout/SizedElementComponent.h"
+#include "donner/svg/components/layout/ViewboxComponent.h"
+
+namespace donner::svg {
+
+SVGTextElement SVGTextElement::CreateOn(EntityHandle handle) {
+  CreateEntityOn(handle, Tag, Type);
+  handle.emplace<components::RenderingBehaviorComponent>(
+      components::RenderingBehavior::NoTraverseChildren);
+
+  return SVGTextElement(handle);
+}
+
+std::optional<Boxd> SVGSVGElement::viewbox() const {
+  return handle_.get<components::ViewboxComponent>().viewbox;
+}
+
+PreserveAspectRatio SVGSVGElement::preserveAspectRatio() const {
+  return handle_.get<components::PreserveAspectRatioComponent>().preserveAspectRatio;
+}
+
+Lengthd SVGSVGElement::x() const {
+  return handle_.get<components::SizedElementComponent>().properties.x.getRequired();
+}
+
+Lengthd SVGSVGElement::y() const {
+  return handle_.get<components::SizedElementComponent>().properties.y.getRequired();
+}
+
+std::optional<Lengthd> SVGSVGElement::width() const {
+  return handle_.get<components::SizedElementComponent>().properties.width.getRequired();
+}
+
+std::optional<Lengthd> SVGSVGElement::height() const {
+  return handle_.get<components::SizedElementComponent>().properties.height.getRequired();
+}
+
+void SVGSVGElement::setViewbox(std::optional<Boxd> viewbox) {
+  handle_.get<components::ViewboxComponent>().viewbox = viewbox;
+}
+
+void SVGSVGElement::setPreserveAspectRatio(PreserveAspectRatio preserveAspectRatio) {
+  handle_.get<components::PreserveAspectRatioComponent>().preserveAspectRatio = preserveAspectRatio;
+}
+
+void SVGSVGElement::setX(Lengthd value) {
+  handle_.get<components::SizedElementComponent>().properties.x.set(value,
+                                                                    css::Specificity::Override());
+}
+
+void SVGSVGElement::setY(Lengthd value) {
+  handle_.get<components::SizedElementComponent>().properties.y.set(value,
+                                                                    css::Specificity::Override());
+}
+
+void SVGSVGElement::setWidth(std::optional<Lengthd> value) {
+  handle_.get<components::SizedElementComponent>().properties.width.set(
+      value, css::Specificity::Override());
+}
+
+void SVGSVGElement::setHeight(std::optional<Lengthd> value) {
+  handle_.get<components::SizedElementComponent>().properties.height.set(
+      value, css::Specificity::Override());
+}
+
+}  // namespace donner::svg
