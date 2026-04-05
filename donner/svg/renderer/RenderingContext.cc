079d1fa3feaab69c58ae11a3fa98148a7e16d766
Jeff McGlynn <jeff@mcglynn.dev>
2026-04-04 22:56:37 -0700

    Revert incomplete clipPath use support to fix CI

--- donner/svg/renderer/RenderingContext.cc
+++ donner/svg/renderer/RenderingContext.cc
@@ -385,32 +385,8 @@ public:
       }
     }
 
-    donner::components::ForAllChildren(clipPathHandle, [&](EntityHandle child) {
-      appendClipPathFromEntity(child);
-
-      const auto* typeComponent = child.try_get<ElementTypeComponent>();
-      if (!typeComponent || typeComponent->type() != ElementType::Use) {
-        return;
-      }
-
-      const auto* useStyle = child.try_get<components::ComputedStyleComponent>();
-      if (!useStyle) {
-        return;
-      }
-
-      const auto& useProperties = useStyle->properties.value();
-      if (useProperties.visibility.getRequired() != Visibility::Visible ||
-          useProperties.display.getRequired() == Display::None) {
-        return;
-      }
-
-      if (const auto* computedShadow = child.try_get<ComputedShadowTreeComponent>();
-          computedShadow && computedShadow->mainBranch) {
-        for (const Entity shadowEntity : computedShadow->mainBranch->shadowEntities) {
-          appendClipPathFromEntity(EntityHandle(registry_, shadowEntity));
-        }
-      }
-    });
+    donner::components::ForAllChildren(
+        clipPathHandle, [&](EntityHandle child) { appendClipPathFromEntity(child); });
 
     return hasAnyChildren;
   }
