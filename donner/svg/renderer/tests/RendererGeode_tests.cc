25dce75045a7b0ad95ef628b1bfe626a0ca48026
Jeff McGlynn <jeff@mcglynn.dev>
2026-09-09 09:07:45 -0700

    Test filter parameters across overlapping renderer frames

--- donner/svg/renderer/tests/RendererGeode_tests.cc
+++ donner/svg/renderer/tests/RendererGeode_tests.cc
@@ -315,6 +315,45 @@ protected:
 
 // ----------------------------------------------------------------------------
 
+TEST_F(RendererGeodeTest, OverlappingFiltersPreserveEachFramesParameters) {
+  for (const bool parentFirst : {false, true}) {
+    SCOPED_TRACE(parentFirst);
+    const std::shared_ptr<geode::GeodeDevice> device = geode::GeodeDevice::CreateHeadless();
+    ASSERT_THAT(device, testing::NotNull());
+    RendererGeode parent(device);
+    RendererGeode sibling(device);
+    const auto drawFlood = [](RendererGeode& renderer, css::RGBA color) {
+      components::FilterGraph graph;
+      graph.colorInterpolationFilters = ColorInterpolationFilters::SRGB;
+      components::FilterNode node;
+      node.primitive = components::filter_primitive::Flood{.floodColor = css::Color(color)};
+      graph.nodes.push_back(node);
+      renderer.pushFilterLayer(graph, Box2d({0, 0}, {kViewportSize, kViewportSize}));
+      renderer.popFilterLayer();
+    };
+    beginFrame(parent);
+    drawFlood(parent, css::RGBA(255, 0, 0, 255));
+    beginFrame(sibling);
+    drawFlood(sibling, css::RGBA(0, 0, 255, 255));
+    if (parentFirst) {
+      parent.endFrame();
+      sibling.endFrame();
+    } else {
+      sibling.endFrame();
+      parent.endFrame();
+    }
+    const RendererBitmap parentPixels = parent.takeSnapshot();
+    const RendererBitmap siblingPixels = sibling.takeSnapshot();
+    ASSERT_THAT(parentPixels.dimensions, testing::Eq(Vector2i(kViewportSize, kViewportSize)));
+    ASSERT_THAT(siblingPixels.dimensions, testing::Eq(parentPixels.dimensions));
+    for (const int coordinate : {1, 32, 62}) {
+      SCOPED_TRACE(coordinate);
+      EXPECT_THAT(pixelAt(parentPixels, coordinate, coordinate), Rgba(255, 0, 0, 255));
+      EXPECT_THAT(pixelAt(siblingPixels, coordinate, coordinate), Rgba(0, 0, 255, 255));
+    }
+  }
+}
+
 TEST_F(RendererGeodeTest, OverlappingFramesKeepReplayWithTheirOwner) {
   const std::shared_ptr<geode::GeodeDevice> device = geode::GeodeDevice::CreateHeadless();
   ASSERT_THAT(device, testing::NotNull());
